c7b4739a1a1bf7be7dfb542cd779cd044ba0d2a1
Leonid <78638629+theheleo@users.noreply.github.com>
2024-10-22 17:16:51 +0300

    initial

---
+++ myTest/myTest/myTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  myTestApp.swift
+//  myTest
+//
+//  Created by Леонид  Егоров on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct myTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
